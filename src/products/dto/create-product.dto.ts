20b00419f02b00527ca9b7481c635c519053e786
MiloVirus <camilovalencia.dev@gmail.com>
2025-09-30 22:28:14 -0500

    First Commit

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,14 @@
+import { Type } from "class-transformer";
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    public name: string;
+
+    @IsString()
+    public description: string;
+
+    @IsNumber()
+    @Type(() => Number)
+    public price: string;
+}
